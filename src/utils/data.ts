2c6a8e6a58a61ae17e2e8f4d5a4932fe81b69b88
davidncrdev <davidkabre25@gmail.com>
2024-06-24 01:31:19 +0200

    todo

---
+++ src/utils/data.ts
@@ -0,0 +1,34 @@
+const services = [
+    {
+        title: "Cultos Dominicales",
+        description: "Únete a nosotros cada domingo para adorar y recibir la palabra de Dios en un ambiente de amor y fraternidad.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /> </svg>`,
+    },
+    {
+        title: "Grupos de Estudio Bíblico",
+        description: "Participa en nuestros grupos de estudio bíblico y profundiza en el conocimiento de las Escrituras.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /> </svg>`,
+    },
+    {
+        title: "Consejería Espiritual",
+        description: "Ofrecemos consejería espiritual para ayudarte a enfrentar los desafíos de la vida con fe y esperanza.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /> </svg>`,
+    },
+    {
+        title: "Actividades Juveniles",
+        description: "Involucra a los jóvenes en nuestras actividades diseñadas para fortalecer su fe y construir comunidad.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /> </svg>`,
+    },
+    {
+        title: "Servicio de Alabanza",
+        description: "Únete a nuestro ministerio de alabanza y glorifica a Dios con música y canto.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /> </svg>`,
+    },
+    {
+        title: "Eventos Especiales",
+        description: "Participa en nuestros eventos especiales que se realizan durante todo el año para la edificación de la comunidad.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /> </svg>`,
+    },
+];
+
+export { services };
